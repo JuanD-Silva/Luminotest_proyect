9e16ec2a42ddbac70096249f7d5ed2bab5164fef
Juan Silva <silvajuandavid.08@gmail.com>
2023-03-17 13:07:09 -0500

    inicio estilos del pdf

--- src/controllers/pdfsController.js
+++ src/controllers/pdfsController.js
@@ -1,9 +1,11 @@
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
 
+
+
 function generatePDF(req, res) {
     req.getConnection((err, conn) => {
-        conn.query('SELECT * FROM users', (err, data) => {
+        conn.query('SELECT * FROM formulario', (err, data) => {
           if (err) {
             Console.log('Error')
             throw err; 
@@ -12,10 +14,20 @@ function generatePDF(req, res) {
         const doc = new PDFDocument();
         const stream = doc.pipe(fs.createWriteStream('documento.pdf'));
 
+        const blueColor = '#7DD2E4';
+        const secondaryColor = '#36C0E1';
+        
+        doc.polygon([10, 50], [100 + 10, 50], [200 + 10, 0], [10, 0]).fill(blueColor);
+        doc.polygon([200 + 10, 0], [290 + 10, 0], [250 + 10, 50], [160 + 10, 50]).fill(secondaryColor);
+
+
+
+        doc.translate(10, 60);
         data.forEach((item) => {
-            console.log(`${item.name}`)
-            doc.text(`Nombre: ${item.name}`);
-            doc.text(`Email: ${item.email}`);
+            console.log(`${item["campo-texto"]}`);
+            doc.text(`mensaje ${item["campo-texto"]}`);
+            doc.text(`imagen ${item["campo-imagen"]}`);
+            doc.text(`estado ${item["campo-seleccion"]}`);
             doc.text('-----------------------------------------');
           });
           doc.end();
@@ -28,6 +40,23 @@ function generatePDF(req, res) {
       });
     }
 
+    function insertData(req, res) {
+      const data = req.body;
+      req.getConnection((err, conn) => {
+      conn.query('INSERT INTO formulario SET ?', [data], (err, rows) => {
+      if (err) {
+      console.log('Error inserting data: ', err);
+      res.sendStatus(500);
+      } else {
+      console.log('Data inserted successfully');
+      res.sendStatus(200);
+      
+            }
+          })
+        })
+      }
+
     module.exports = {
-        generatePDF
+        generatePDF,
+        insertData
     }
--- src/routes/pdfs.js
+++ src/routes/pdfs.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const pdfController = require('../controllers/pdfsController');
 
+
 const routerPdf = express.Router();
 
 routerPdf.get('/generate-pdf', pdfController.generatePDF);
+routerPdf.post('/insert-data', pdfController.insertData);
 
 module.exports = routerPdf;
